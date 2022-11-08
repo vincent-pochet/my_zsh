32f41bb6e32bfd104bb52d5298e939d01ddff585
Vincent Pochet <vincent.pochet@gmail.com>
2022-11-08 09:07:32 +0100

    Cleanup legacy aliases

--- lib/rails.zsh
+++
@@ -1,7 +0,0 @@
-alias rtest="rake test"
-alias rctest="COVERAGE=true bundle exec rake test"
-alias rbtest="ruby -Itest"
-alias i18n="rake i18n:js:export"
-alias remigrate="bundle exec rake db:remigrate"
-
-alias spuma="bundle exec puma -e development -b unix:///$HOME/code/shared/puma.sock"
